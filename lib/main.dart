1c95378da80726857a2f3a13c0c2a3cd76932992
Michal <majchrzak-michal@outlook.com>
2021-08-25 22:54:06 +0200

    Convert Home view to Stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("App Bar Widget"),
-      centerTitle: true,
-      backgroundColor: Colors.deepPurpleAccent,
-    ),
-    body: Center(
-      child: Text(
-        "Text widget\ninside\nCenter widget",
-        style: TextStyle(
-          color: Colors.lightGreen[800],
-          fontFamily: 'IndieFlower',
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
+  home: Home(),
+));
+
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App Bar Widget"),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Center(
+        child: Text(
+          "Text widget\ninside\nCenter widget",
+          style: TextStyle(
+            color: Colors.lightGreen[800],
+            fontFamily: 'IndieFlower',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text("Button"),
-      backgroundColor: Colors.deepPurpleAccent,
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Button"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+    );
+  }
+}
