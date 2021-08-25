46768d323ba921d453073a17435d76c110e1cffa
Michal <majchrzak-michal@outlook.com>
2021-08-25 21:58:34 +0200

    Add Scaffold Widget
    Add Scaffold widget for general app layout.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text("Hey World!"),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("App Bar Widget"),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text("Text Widget inside Center Widget"),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text("Button"),
+    ),
+  ),
 ));
